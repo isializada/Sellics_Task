9c7500dae39db2a4b9d1141455f0ee0b9989fa96
ismayilalizada <ismail.alizadeh@yandex.com>
2020-01-19 17:24:51 +0100

    Estimation service | implemented 10 seconds timeout on controller

--- src/main/java/com/alizada/sellics/controller/ScoreController.java
+++ src/main/java/com/alizada/sellics/controller/ScoreController.java
@@ -4,9 +4,14 @@ import com.alizada.sellics.model.SearchValuesEstimation;
 import com.alizada.sellics.service.AmazonAutocompleteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.WebAsyncTask;
+
+import java.util.concurrent.Callable;
 
 @RestController
 public class ScoreController {
+    private static final int TIMEOUT_MILLIS = 10000;
+
     private AmazonAutocompleteService amazonAutocomplete;
 
     @Autowired
@@ -15,8 +20,8 @@ public class ScoreController {
     }
 
     @RequestMapping(value = "/estimate", method = RequestMethod.GET)
-    public SearchValuesEstimation estimate(@RequestParam(value = "keyword", defaultValue = "") String keyword){
-        return amazonAutocomplete.estimateSearchVolume(keyword);
+    public WebAsyncTask<SearchValuesEstimation> estimate(@RequestParam(value = "keyword", defaultValue = "") String keyword){
+        Callable<SearchValuesEstimation> callable = () -> amazonAutocomplete.estimateSearchVolume(keyword);
+        return new WebAsyncTask<>(TIMEOUT_MILLIS, callable);
     }
-
 }
