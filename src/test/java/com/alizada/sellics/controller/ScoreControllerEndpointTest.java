84de484f5c87e9ccf5ab657ceaa80e63991884b0
ismayilalizada <ismail.alizadeh@yandex.com>
2020-01-19 17:25:26 +0100

    Estimation service | unit tests updated and deleted not needed endpoint test case

---
+++ src/test/java/com/alizada/sellics/controller/ScoreControllerEndpointTest.java
@@ -0,0 +1,46 @@
+package com.alizada.sellics.controller;
+
+import com.alizada.sellics.service.AmazonAutocompleteService;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class ScoreControllerEndpointTest {
+
+    @Autowired
+    private AmazonAutocompleteService amazonAutocompleteService;
+
+    @Autowired
+    WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void shouldReturn200Status() throws Exception {
+        String keyword = "bluedio t5";
+
+        this.mockMvc.perform(get("/estimate?keyword=" + keyword))
+                .andDo(print()).andExpect(status().isOk());
+    }
+
+}
